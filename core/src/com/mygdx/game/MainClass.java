11d00e60aa72e37725adb11de68c1fa6cd949054
Ilya Vasilchenko <lawwwer@yahoo.com>
2016-03-23 18:15:42 +0300

    ArrayList<> for tanks is better. =)

--- core/src/com/mygdx/game/MainClass.java
+++ core/src/com/mygdx/game/MainClass.java
@@ -7,11 +7,13 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainClass extends ApplicationAdapter {
 	SpriteBatch batch;
-	public BaseTank[] tanks = new BaseTank[15];
+	ArrayList<BaseTank> tanks = new ArrayList<BaseTank>();
+	int countOfTanks = 20;
 	Texture mainBackground;
 	Texture secondBackground;
 	Random rand = new Random();
@@ -21,9 +23,9 @@ public class MainClass extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		mainBackground = new Texture("font2.png");
 		secondBackground = new Texture("font.png");
-		tanks[0] = new PlayerTank(new Vector2(Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/2 ));
-		for (int i = 1; i < tanks.length; i++) {
-			tanks[i] = new BotWacher(new Vector2(rand.nextInt(1000 - tanks[0].myTexture.getHeight()),rand.nextInt(600 - tanks[0].myTexture.getHeight())));
+		tanks.add(0, new PlayerTank(new Vector2(Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/2 )));
+		for (int i = 0; i < countOfTanks; i++) {
+			tanks.add(new BotWacher(new Vector2(rand.nextInt(1000 - tanks.get(0).myTexture.getHeight()),rand.nextInt(600 - tanks.get(0).myTexture.getHeight()))));
 		}
 	}
 
@@ -36,29 +38,29 @@ public class MainClass extends ApplicationAdapter {
 		batch.draw(mainBackground,0,0,1000,600);
 		for (int i = 0; i < 600; i+=100)
 			batch.draw(secondBackground,1000,i,100,100);
-		for (int i = 0; i < tanks.length; i++) {
-			if(tanks[i].isAlive())
-				tanks[i].draw(batch);
+		for (int i = 0; i < tanks.size(); i++) {
+			if(tanks.get(i).isAlive())
+				tanks.get(i).draw(batch);
 		}
 		batch.end();
 	}
 
 	public void update(){
-		tanks[0].update();
-		for (int i = 1; i < tanks.length; i++) {
-			if(tanks[i].isAlive()) {
-				tanks[i].update();
-				float lenTanks = (float)Math.sqrt(Math.pow(tanks[0].position.x - tanks[i].position.x, 2) +
-						Math.pow(tanks[0].position.y - tanks[i].position.y, 2));
+		tanks.get(0).update();
+		for (int i = 1; i < tanks.size(); i++) {
+			if(tanks.get(i).isAlive()) {
+				tanks.get(i).update();
+				float lenTanks = (float)Math.sqrt(Math.pow(tanks.get(0).position.x - tanks.get(i).position.x, 2) +
+						Math.pow(tanks.get(0).position.y - tanks.get(i).position.y, 2));
 				if(lenTanks < 35){
-					tanks[i].destroy();
+					tanks.get(i).destroy();
 				}
-				for (int j = 0; j < tanks[0].ams.size(); j++) {
-					float lenBullet = (float)Math.sqrt(Math.pow(tanks[0].ams.get(j).position.x - tanks[i].position.x, 2) +
-							Math.pow(tanks[0].ams.get(j).position.y - tanks[i].position.y, 2));
+				for (int j = 0; j < tanks.get(0).ams.size(); j++) {
+					float lenBullet = (float)Math.sqrt(Math.pow(tanks.get(0).ams.get(j).position.x - tanks.get(i).position.x, 2) +
+							Math.pow(tanks.get(0).ams.get(j).position.y - tanks.get(i).position.y, 2));
 					if(lenBullet < 30){
-						tanks[i].destroy();
-						tanks[0].ams.remove(j);
+						tanks.get(i).destroy();
+						tanks.get(0).ams.remove(j);
 					}
 				}
 			}
