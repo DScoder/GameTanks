a97833ac5a6519277f95d5146041381cdea12a93
Ilya Vasilchenko <lawwwer@yahoo.com>
2016-03-25 17:52:49 +0300

    Desctop = 1100x600

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,8 +8,10 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "DScoder's TANKS";
-        config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
-        config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
+        config.width = 1100;
+        config.height = 600;
+//        config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
+//        config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
         config.resizable = false;
         new LwjglApplication(new MainClass(), config);
     }
